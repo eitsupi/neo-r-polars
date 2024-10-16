2263f7456d9d1818e90c73ce0febd964c290871a
etiennebacher <etienne.bacher@protonmail.com>
2024-10-16 15:47:18 +0200

    deprecate set_ordering

--- tests/testthat/test-expr-categorical.R
+++ tests/testthat/test-expr-categorical.R
@@ -1,17 +1,26 @@
 test_that("set_ordering", {
   dat <- pl$DataFrame(x = c("z", "z", "k", "a", "b"))$cast(pl$Categorical())
+  # TODO: remove before next release
+  expect_warning(
+    expect_equal(
+      dat$with_columns(pl$col("x")$cat$set_ordering("physical")$sort()),
+      pl$DataFrame(x = factor(c("z", "z", "k", "a", "b")))
+    ),
+    "deprecated"
+  )
+
   expect_equal(
-    dat$with_columns(pl$col("x")$cat$set_ordering("physical")$sort()),
+    dat$with_columns(pl$col("x")$cast(pl$Categorical("physical"))$sort()),
     pl$DataFrame(x = factor(c("z", "z", "k", "a", "b")))
   )
 
   expect_equal(
-    dat$with_columns(pl$col("x")$cat$set_ordering("lexical")$sort()),
+    dat$with_columns(pl$col("x")$cast(pl$Categorical("lexical"))$sort()),
     pl$DataFrame(x = factor(c("a", "b", "k", "z", "z")))$cast(pl$Categorical("lexical"))
   )
 
   expect_snapshot(
-    dat$with_columns(pl$col("x")$cat$set_ordering("foo")$sort()),
+    dat$with_columns(pl$col("x")$cast(pl$Categorical("foo"))$sort()),
     error = TRUE
   )
 })
