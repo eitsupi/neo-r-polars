122aea47b02da823f21304ab2c304d668a82af21
etiennebacher <etienne.bacher@protonmail.com>
2024-10-30 22:15:46 +0100

    add _horizontal functions

---
+++ R/functions-aggregation-horizontal.R
@@ -0,0 +1,145 @@
+#' Get the minimum value horizontally across columns
+#'
+#' @param ... <[`dynamic-dots`][rlang::dyn-dots]> Columns to concatenate into a
+#' single string column. Accepts expressions. Strings are parsed as column
+#' names, other non-expression inputs are parsed as literals.
+#'
+#' @inherit as_polars_expr return
+#'
+#' @examples
+#' df <- pl$DataFrame(
+#'   a = c(1, 8, 3)
+#'   b = c(4, 5, NA),
+#'   c = c("x", "y", "z")
+#' )
+#' df$with_columns(
+#'   min = pl$min_horizontal("a", "b")
+#' )
+pl__min_horizontal <- function(...) {
+  wrap({
+    check_dots_unnamed()
+    by <- parse_into_list_of_expressions(...)
+    min_horizontal(by)
+  })
+}
+
+#' Get the maximum value horizontally across columns
+#'
+#' @inheritParams pl_min_horizontal
+#' @inherit as_polars_expr return
+#'
+#' @examples
+#' df <- pl$DataFrame(
+#'   a = c(1, 8, 3)
+#'   b = c(4, 5, NA),
+#'   c = c(1, 2, NA, Inf)
+#' )
+#' df$with_columns(
+#'   max = pl$max_horizontal("a", "b")
+#' )
+pl__max_horizontal <- function(...) {
+  wrap({
+    check_dots_unnamed()
+    by <- parse_into_list_of_expressions(...)
+    max_horizontal(by)
+  })
+}
+
+#' Apply the AND logical horizontally across columns
+#'
+#' @inheritParams pl_min_horizontal
+#'
+#' @details
+#' [Kleene logic](https://en.wikipedia.org/wiki/Three-valued_logic) is used to
+#' deal with nulls: if the column contains any null values and no `FALSE`
+#' values, the output is null.
+#'
+#' @inherit as_polars_expr return
+#'
+#' @examples
+#' df <- pl$DataFrame(
+#'   a = c(FALSE, FALSE, TRUE, TRUE, FALSE, NA),
+#'   b = c(FALSE, TRUE, TRUE, NA, NA, NA),
+#'   c = c("u", "v", "w", "x", "y", "z")
+#' )
+#'
+#' df$with_columns(
+#'   all = pl$all_horizontal("a", "b", "c")
+#' )
+pl__all_horizontal <- function(...) {
+  wrap({
+    check_dots_unnamed()
+    by <- parse_into_list_of_expressions(...)
+    all_horizontal(by)
+  })
+}
+
+#' Apply the OR logical horizontally across columns
+#'
+#' @inheritParams pl_min_horizontal
+#' @inherit pl_all_horizontal details
+#' @inherit as_polars_expr return
+#'
+#' @examples
+#' df <- pl$DataFrame(
+#'   a = c(FALSE, FALSE, TRUE, TRUE, FALSE, NA),
+#'   b = c(FALSE, TRUE, TRUE, NA, NA, NA),
+#'   c = c("u", "v", "w", "x", "y", "z")
+#' )
+#'
+#' df$with_columns(
+#'   any = pl$any_horizontal("a", "b", "c")
+#' )
+pl__any_horizontal <- function(...) {
+  wrap({
+    check_dots_unnamed()
+    by <- parse_into_list_of_expressions(...)
+    any_horizontal(by)
+  })
+}
+
+#' Compute the sum horizontally across columns
+#'
+#' @inheritParams pl_min_horizontal
+#' @inherit as_polars_expr return
+#'
+#' @examples
+#' df <- pl$DataFrame(
+#'   a = c(1, 8, 3)
+#'   b = c(4, 5, NA),
+#'   c = c("x", "y", "z")
+#' )
+#' df$with_columns(
+#'   sum = pl$sum_horizontal("a", "b")
+#' )
+pl__sum_horizontal <- function(...) {
+  wrap({
+    check_dots_unnamed()
+    by <- parse_into_list_of_expressions(...)
+    sum_horizontal(by)
+  })
+}
+
+#' Compute the mean horizontally across columns
+#'
+#' @inheritParams pl_min_horizontal pl_sum_horizontal
+#'
+#' @inherit as_polars_expr return
+#'
+#' @examples
+#' df <- pl$DataFrame(
+#'   a = c(1, 8, 3)
+#'   b = c(4, 5, NA),
+#'   c = c("x", "y", "z")
+#' )
+#'
+#' df$with_columns(
+#'   mean = pl$mean_horizontal("a", "b")
+#' )
+pl__mean_horizontal <- function(...) {
+  wrap({
+    check_dots_unnamed()
+    by <- parse_into_list_of_expressions(...)
+    mean_horizontal(by)
+  })
+}
