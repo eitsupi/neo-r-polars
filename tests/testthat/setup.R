6b4cc9c8410bee79efe45a5fba3d7227ded85d28
etiennebacher <etienne.bacher@protonmail.com>
2024-11-23 22:42:46 +0100

    rename files

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,21 +1,23 @@
 Sys.setenv(POLARS_IN_LAZY_TEST = FALSE)
 
-to_duplicate <- test_path("test-frame.R")
+orig <- test_path("test-dataframe-frame.R")
+dest <- test_path("test-lazyframe-frame.R")
 
-for (i in to_duplicate) {
-  tmp <- readLines(i)
-  out <- gsub("pl\\$DataFrame", "pl\\$LazyFrame", tmp)
-  out <- gsub("as_polars_df\\(", "as_polars_lf(", out)
-  out <- gsub("expect_equal\\(", "expect_equal_lazy(", out)
-  out <- gsub("expect_error\\(", "expect_error_lazy(", out)
-  out <- gsub("expect_snapshot", "expect_snapshot_lazy", out)
-  out <- paste0(
-    "#############################################\n",
-    "### [GENERATED AUTOMATICALLY] Update ", i, " instead.\n",
-    "#############################################\n\n",
-    "Sys.setenv(POLARS_IN_LAZY_TEST = TRUE)\n\n",
-    paste(out, collapse = "\n"),
-    "\n\nSys.setenv(POLARS_IN_LAZY_TEST = FALSE)"
-  )
-  cat(out, file = gsub("\\.R$", "-lazy\\.R", i))
-}
+tmp <- readLines(orig)
+out <- gsub("pl\\$DataFrame", "pl\\$LazyFrame", tmp)
+out <- gsub("as_polars_df\\(", "as_polars_lf(", out)
+out <- gsub("expect_equal\\(", "expect_equal_lazy(", out)
+out <- gsub("expect_error\\(", "expect_error_lazy(", out)
+out <- gsub("expect_snapshot", "expect_snapshot_lazy", out)
+out <- paste0(
+  "#############################################\n",
+  "### [GENERATED AUTOMATICALLY] Update ", orig, " instead.\n",
+  "#############################################\n\n",
+  "withr::with_envvar(
+    list(POLARS_IN_LAZY_TEST = TRUE),
+    {\n",
+  paste(out, collapse = "\n"),
+  "\n}
+  )"
+)
+cat(out, file = dest)
--- tests/testthat/test-lazyframe-frame.R
+++ tests/testthat/test-lazyframe-frame.R
@@ -1,42 +1,49 @@
+#############################################
+### [GENERATED AUTOMATICALLY] Update test-dataframe-frame.R instead.
+#############################################
+
+withr::with_envvar(
+    list(POLARS_IN_LAZY_TEST = TRUE),
+    {
 patrick::with_parameters_test_that(
-  "use pl$DataFrame() to construct a DataFrame",
+  "use pl$LazyFrame() to construct a DataFrame",
   .cases = {
     tibble::tribble(
       ~.test_name, ~object, ~expected,
-      "simple", pl$DataFrame(a = 1, b = list("b"), ), as_polars_df(list(a = 1, b = list("b"))),
-      "!!! for list", pl$DataFrame(!!!list(a = 1, b = list("b")), c = 1), as_polars_df(list(a = 1, b = list("b"), c = 1)),
-      "!!! for data.frame", pl$DataFrame(!!!data.frame(a = 1, b = "b"), c = 1), as_polars_df(list(a = 1, b = "b", c = 1)),
-      "empty", pl$DataFrame(), as_polars_df(list()),
+      "simple", pl$LazyFrame(a = 1, b = list("b"), ), as_polars_lf(list(a = 1, b = list("b"))),
+      "!!! for list", pl$LazyFrame(!!!list(a = 1, b = list("b")), c = 1), as_polars_lf(list(a = 1, b = list("b"), c = 1)),
+      "!!! for data.frame", pl$LazyFrame(!!!data.frame(a = 1, b = "b"), c = 1), as_polars_lf(list(a = 1, b = "b", c = 1)),
+      "empty", pl$LazyFrame(), as_polars_lf(list()),
     )
   },
   code = {
-    expect_equal(object, expected)
+    expect_equal_lazy(object, expected)
   }
 )
 
-test_that("pl$DataFrame() requires series the same length", {
-  expect_error(pl$DataFrame(a = 1:2, b = "foo"), "has length 2")
+test_that("pl$LazyFrame() requires series the same length", {
+  expect_error_lazy(pl$LazyFrame(a = 1:2, b = "foo"), "has length 2")
 })
 
-test_that("pl$DataFrame() rejects expressions", {
-  expect_error(
-    pl$DataFrame(a = 1:2, b = pl$lit("foo")),
+test_that("pl$LazyFrame() rejects expressions", {
+  expect_error_lazy(
+    pl$LazyFrame(a = 1:2, b = pl$lit("foo")),
     r"(Try evaluating the expression first using `pl\$select\(\)`)"
   )
 })
 
 test_that("to_struct()", {
   skip_if(is_in_lazy_test())
-  expect_equal(
-    as_polars_df(mtcars)$to_struct("foo"),
+  expect_equal_lazy(
+    as_polars_lf(mtcars)$to_struct("foo"),
     as_polars_series(mtcars, "foo")
   )
 })
 
 test_that("get_columns()", {
   skip_if(is_in_lazy_test())
-  expect_equal(
-    pl$DataFrame(a = 1:2, b = c("foo", "bar"))$get_columns(),
+  expect_equal_lazy(
+    pl$LazyFrame(a = 1:2, b = c("foo", "bar"))$get_columns(),
     list(
       a = as_polars_series(1:2, "a"),
       b = as_polars_series(c("foo", "bar"), "b")
@@ -51,39 +58,39 @@ test_that("to_series()", {
     b = c("foo", "bar")
   )
 
-  expect_equal(
-    as_polars_df(data)$to_series(),
+  expect_equal_lazy(
+    as_polars_lf(data)$to_series(),
     as_polars_series(data$a, "a")
   )
-  expect_equal(
-    as_polars_df(data)$to_series(1),
+  expect_equal_lazy(
+    as_polars_lf(data)$to_series(1),
     as_polars_series(data$b, "b")
   )
 })
 
 test_that("select works lazy/eager", {
-  .data <- pl$DataFrame(
+  .data <- pl$LazyFrame(
     int32 = 1:5,
     int64 = as_polars_series(1:5)$cast(pl$Int64),
     string = letters[1:5],
   )
 
-  expect_equal(
+  expect_equal_lazy(
     .data$select("int32"),
-    pl$DataFrame(int32 = 1:5)
+    pl$LazyFrame(int32 = 1:5)
   )
-  expect_equal(
+  expect_equal_lazy(
     .data$select(pl$lit("int32")),
-    pl$DataFrame(literal = "int32")
+    pl$LazyFrame(literal = "int32")
   )
-  expect_equal(
+  expect_equal_lazy(
     .data$select(foo = "int32"),
-    pl$DataFrame(foo = 1:5)
+    pl$LazyFrame(foo = 1:5)
   )
 })
 
 test_that("POLARS_AUTO_STRUCTIFY works for select", {
-  .data <- pl$DataFrame(
+  .data <- pl$LazyFrame(
     foo = 1:3,
     bar = 6:8,
     ham = letters[1:3],
@@ -92,7 +99,7 @@ test_that("POLARS_AUTO_STRUCTIFY works for select", {
   withr::with_envvar(
     c(POLARS_AUTO_STRUCTIFY = "foo"),
     {
-      expect_error(
+      expect_error_lazy(
         .data$select(1),
         r"(Environment variable `POLARS_AUTO_STRUCTIFY` must be one of \('0', '1'\), got 'foo')"
       )
@@ -102,12 +109,12 @@ test_that("POLARS_AUTO_STRUCTIFY works for select", {
   withr::with_envvar(
     c(POLARS_AUTO_STRUCTIFY = "0"),
     {
-      expect_error(
+      expect_error_lazy(
         .data$select(is_odd = ((pl$col(pl$Int32) %% 2) == 1)$name$suffix("_is_odd")),
         "`keep`, `suffix`, `prefix` should be last expression"
       )
 
-      expect_equal(
+      expect_equal_lazy(
         withr::with_envvar(c(POLARS_AUTO_STRUCTIFY = "1"), {
           .data$select(is_odd = ((pl$col(pl$Int32) %% 2) == 1)$name$suffix("_is_odd"))
         }),
@@ -120,86 +127,86 @@ test_that("POLARS_AUTO_STRUCTIFY works for select", {
 })
 
 test_that("slice/head/tail work", {
-  .data <- pl$DataFrame(
+  .data <- pl$LazyFrame(
     foo = 1:5,
     bar = 6:10,
   )
 
   # slice
-  expect_equal(
+  expect_equal_lazy(
     .data$slice(1),
-    pl$DataFrame(foo = 2:5, bar = 7:10)
+    pl$LazyFrame(foo = 2:5, bar = 7:10)
   )
-  expect_equal(
+  expect_equal_lazy(
     .data$slice(1, 2),
-    pl$DataFrame(foo = 2:3, bar = 7:8)
+    pl$LazyFrame(foo = 2:3, bar = 7:8)
   )
-  expect_equal(
+  expect_equal_lazy(
     .data$slice(1, 2),
-    pl$DataFrame(foo = 2:3, bar = 7:8)
+    pl$LazyFrame(foo = 2:3, bar = 7:8)
   )
-  expect_equal(
+  expect_equal_lazy(
     .data$slice(4, 100),
-    pl$DataFrame(foo = 5L, bar = 10L)
+    pl$LazyFrame(foo = 5L, bar = 10L)
   )
   if (is_in_lazy_test()) {
-    expect_error(
+    expect_error_lazy(
       .data$slice(0, -2),
       r"(-2.0 is out of range that can be safely converted to u32)"
     )
   } else {
-    expect_equal(
+    expect_equal_lazy(
       .data$slice(0, -2),
-      pl$DataFrame(foo = 1:3, bar = 6:8)
+      pl$LazyFrame(foo = 1:3, bar = 6:8)
     )
   }
 
   # head
-  expect_equal(
+  expect_equal_lazy(
     .data$head(1),
-    pl$DataFrame(foo = 1L, bar = 6L)
+    pl$LazyFrame(foo = 1L, bar = 6L)
   )
-  expect_equal(
+  expect_equal_lazy(
     .data$head(100),
     .data
   )
   if (is_in_lazy_test()) {
-    expect_error(
+    expect_error_lazy(
       .data$head(-4),
       r"(-4.0 is out of range that can be safely converted to u32)"
     )
   } else {
-    expect_equal(
+    expect_equal_lazy(
       .data$head(-4),
-      pl$DataFrame(foo = 1L, bar = 6L)
+      pl$LazyFrame(foo = 1L, bar = 6L)
     )
   }
 
   # tail
-  expect_equal(
+  expect_equal_lazy(
     .data$tail(1),
-    pl$DataFrame(foo = 5L, bar = 10L)
+    pl$LazyFrame(foo = 5L, bar = 10L)
   )
-  expect_equal(
+  expect_equal_lazy(
     .data$tail(100),
     .data
   )
   if (is_in_lazy_test()) {
-    expect_error(
+    expect_error_lazy(
       .data$tail(-4),
       r"(-4\.0 is out of range that can be safely converted to u32)"
     )
   } else {
-    expect_equal(
+    expect_equal_lazy(
       .data$tail(-4),
-      pl$DataFrame(foo = 5L, bar = 10L)
+      pl$LazyFrame(foo = 5L, bar = 10L)
     )
   }
 })
 
 
 test_that("unnest works correctly", {
-  df <- pl$DataFrame(
+  df <- pl$LazyFrame(
     a = 1:5,
     b = c("one", "two", "three", "four", "five"),
     c = 6:10
@@ -216,14 +223,14 @@ test_that("unnest works correctly", {
   )
 
   # wrong input
-  expect_snapshot(
+  expect_snapshot_lazy(
     df$unnest("b", pl$col("a_and_c")),
     error = TRUE
   )
-  expect_snapshot(df$unnest(1), error = TRUE)
+  expect_snapshot_lazy(df$unnest(1), error = TRUE)
 
   # wrong datatype
-  expect_snapshot(df$unnest("foo"), error = TRUE)
+  expect_snapshot_lazy(df$unnest("foo"), error = TRUE)
 })
 
 
@@ -247,9 +254,11 @@ patrick::with_parameters_test_that(
   "simple translations: eager",
   {
     browser()
-    a <- as_polars_df(mtcars)[[pola]]()
-    b <- as_polars_df(!!!lapply(mtcars, base))
-    expect_equal(a, b)
+    a <- as_polars_lf(mtcars)[[pola]]()
+    b <- as_polars_lf(!!!lapply(mtcars, base))
+    expect_equal_lazy(a, b)
   },
   .cases = make_cases()
 )
+}
+  )
