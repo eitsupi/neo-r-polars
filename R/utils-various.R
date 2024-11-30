4ab8edad1c53d62a70e213887cf6de1889bdc76a
eitsupi <ts1s1andn@gmail.com>
2024-11-30 03:25:13 +0000

    refactor: sync to Python Polars

--- R/utils-various.R
+++ R/utils-various.R
@@ -16,28 +16,3 @@ extend_bool <- function(value, n_match, value_name, match_name) {
     value
   }
 }
-
-#' Used in ewm_* functions
-#' @noRd
-prepare_alpha <- function(com, span, half_life, alpha) {
-  check_exclusive(com, span, half_life, alpha, .call = caller_env())
-
-  if (!missing(com)) {
-    check_number_decimal(com, min = 0)
-    alpha <- 1 / (1 + com)
-  } else if (!missing(span)) {
-    check_number_decimal(span, min = 1)
-    alpha <- 2 / (span + 1)
-  } else if (!missing(half_life)) {
-    check_number_decimal(half_life, min = 0)
-    alpha <- 1 - exp(-log(2) / half_life)
-  } else if (!missing(alpha)) {
-    # Can't use "min" arg in check_number_decimal() since requirement is > 0
-    check_number_decimal(alpha)
-    if (alpha <= 0 || alpha > 1) {
-      abort("`alpha` must be between greater than 0 and lower or equal to 1.", call = caller_env())
-    }
-  }
-
-  alpha
-}
