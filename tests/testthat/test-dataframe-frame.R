ba0b4529572f11e06126c51a07dacfb0d0a6e6e8
etiennebacher <etienne.bacher@protonmail.com>
2025-02-17 18:33:21 +0100

    more tests

--- tests/testthat/test-dataframe-frame.R
+++ tests/testthat/test-dataframe-frame.R
@@ -106,6 +106,26 @@ test_that("to_dummies() works", {
     )$cast(pl$UInt8)
   )
   expect_equal(
+    df$to_dummies(c("foo", "bar")),
+    pl$DataFrame(
+      foo_1 = 1:0,
+      foo_2 = 0:1,
+      bar_3 = 1:0,
+      bar_4 = 0:1,
+      ham = c("a", "b")
+    )$cast(foo_1 = pl$UInt8, foo_2 = pl$UInt8, bar_3 = pl$UInt8, bar_4 = pl$UInt8)
+  )
+  expect_equal(
+    df$to_dummies("foo", "bar"),
+    pl$DataFrame(
+      foo_1 = 1:0,
+      foo_2 = 0:1,
+      bar_3 = 1:0,
+      bar_4 = 0:1,
+      ham = c("a", "b")
+    )$cast(foo_1 = pl$UInt8, foo_2 = pl$UInt8, bar_3 = pl$UInt8, bar_4 = pl$UInt8)
+  )
+  expect_equal(
     df$to_dummies(drop_first = TRUE),
     pl$DataFrame(foo_2 = 0:1, bar_4 = 0:1, ham_b = 0:1)$cast(pl$UInt8)
   )
