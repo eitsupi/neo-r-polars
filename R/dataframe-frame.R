f5d7d1a27b7d041da7fe66392bde1ff51d3cf6cc
etiennebacher <etienne.bacher@protonmail.com>
2025-02-17 08:35:31 +0100

    init

--- R/dataframe-frame.R
+++ R/dataframe-frame.R
@@ -1231,3 +1231,45 @@ dataframe__unpivot <- function(
     )
   })
 }
+
+#' Group by the given columns and return the groups as separate dataframes
+#'
+#' @param ... <[`dynamic-dots`][rlang::dyn-dots]> Column name(s) or selector(s) to group by.
+#' @param maintain_order Ensure that the order of the groups is consistent with
+#' the input data. This is slower than a default partition by operation.
+#' @param include_key Include the columns used to partition the DataFrame in
+#' the output.
+#'
+#' @return A list of DataFrames
+#' @examples
+#' # Pass a single column name to partition by that column.
+#' df <- pl$DataFrame(
+#'   a = c("a", "b", "a", "b", "c"),
+#'   b = c(1, 2, 1, 3, 3),
+#'   c = c(5, 4, 3, 2, 1)
+#' )
+#' df$partition_by("a")
+#'
+#' # Partition by multiple columns:
+#' df$partition_by("a", "b")
+dataframe__partition_by <- function(..., maintain_order = TRUE, include_key = TRUE) {
+  wrap({
+    # TODO: add selectors handling when py-polars' _expand_selectors() has moved
+    # to Rust
+    check_dots_unnamed()
+    by <- c(...)
+    if (length(by) == 0) {
+      abort("`...` must contain at least one column name.")
+    }
+    if (!is_character(by)) {
+      abort("`...` only accepts column names.")
+    }
+    dfs <- self$`_df`$partition_by(
+      by = by,
+      maintain_order = maintain_order,
+      include_key = include_key
+    ) |>
+      wrap()
+    lapply(dfs, \(ptr) .savvy_wrap_PlRDataFrame(ptr) |> wrap())
+  })
+}
