0bbeb5919f963018fd43bf79f205432713bfa61c
eitsupi <ts1s1andn@gmail.com>
2025-02-21 12:54:34 +0000

    pref!: Use custom `$` S3 methods to call py-polars like methods

--- R/datatypes-s3-base.R
+++ R/datatypes-s3-base.R
@@ -1,4 +1,66 @@
 #' @export
+`$.polars_dtype` <- function(x, name) {
+  member_names <- ls(x)
+  method_names <- names(polars_datatype__methods)
+
+  if (name %in% member_names) {
+    get(name, envir = x)
+  } else if (name %in% method_names) {
+    fn <- polars_datatype__methods[[name]]
+    self <- x
+    environment(fn) <- environment()
+    fn
+  } else {
+    NextMethod()
+  }
+}
+
+#' @export
+.DollarNames.polars_dtype <- function(x, pattern = "") {
+  member_names <- ls(x)
+  method_names <- names(polars_datatype__methods)
+
+  all_names <- union(member_names, method_names)
+  filtered_names <- all_names[grep(pattern, all_names)]
+
+  filtered_names[!startsWith(filtered_names, "_")]
+}
+
+#' @export
+`$.polars_dtype_enum` <- function(x, name) {
+  # Enum only method `union`
+  if (identical(name, "union")) {
+    fn <- function(other) {
+      wrap({
+        check_polars_dtype(other)
+        if (!inherits(other, "polars_dtype_enum")) {
+          abort("`other` must be a Enum data type")
+        }
+
+        PlRDataType$new_enum(unique(c(self$categories, other$categories)))
+      })
+    }
+    self <- x
+    environment(fn) <- environment()
+    fn
+  } else {
+    NextMethod()
+  }
+}
+
+#' @export
+.DollarNames.polars_dtype_enum <- function(x, pattern = "") {
+  member_names <- ls(x)
+  # Enum only method `union`
+  method_names <- c("union", names(polars_datatype__methods))
+
+  all_names <- union(member_names, method_names)
+  filtered_names <- all_names[grep(pattern, all_names)]
+
+  filtered_names[!startsWith(filtered_names, "_")]
+}
+
+#' @export
 print.polars_dtype <- function(x, ...) {
   x$`_dt`$print()
   invisible(x)
