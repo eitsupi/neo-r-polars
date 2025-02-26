5026e27c23567261d95893cb7a996dc30edb8741
eitsupi <ts1s1andn@gmail.com>
2025-02-26 11:19:21 +0000

    pref!: Use custom `$` S3 methods to call py-polars like methods

--- R/expr-s3-base.R
+++ R/expr-s3-base.R
@@ -1,4 +1,32 @@
 #' @export
+`$.polars_expr` <- function(x, name) {
+  member_names <- ls(x)
+  method_names <- names(polars_expr__methods)
+
+  if (name %in% member_names) {
+    get(name, envir = x)
+  } else if (name %in% method_names) {
+    fn <- polars_expr__methods[[name]]
+    self <- x
+    environment(fn) <- environment()
+    fn
+  } else {
+    NextMethod()
+  }
+}
+
+#' @export
+`.DollarNames.polars_expr` <- function(x, pattern = "") {
+  member_names <- ls(x)
+  method_names <- names(polars_expr__methods)
+
+  all_names <- union(member_names, method_names)
+  filtered_names <- all_names[grep(pattern, all_names)]
+
+  filtered_names[!startsWith(filtered_names, "_")]
+}
+
+#' @export
 print.polars_expr <- function(x, ...) {
   x$`_rexpr`$as_str() |>
     writeLines()
