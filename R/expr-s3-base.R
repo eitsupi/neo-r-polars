d6131ddc012b8025adf2b4eebfb4205d885f6a41
eitsupi <ts1s1andn@gmail.com>
2025-03-01 11:06:23 +0000

    build: Generate dollar s3 methods via script

--- R/expr-s3-base.R
+++ R/expr-s3-base.R
@@ -1,32 +1,4 @@
 #' @export
-`$.polars_expr` <- function(x, name) {
-  member_names <- ls(x, all.names = TRUE)
-  method_names <- names(polars_expr__methods)
-
-  if (name %in% member_names) {
-    env_get(x, name)
-  } else if (name %in% method_names) {
-    fn <- polars_expr__methods[[name]]
-    self <- x
-    environment(fn) <- environment()
-    fn
-  } else {
-    NextMethod()
-  }
-}
-
-#' @exportS3Method utils::.DollarNames
-.DollarNames.polars_expr <- function(x, pattern = "") {
-  member_names <- ls(x, all.names = TRUE)
-  method_names <- names(polars_expr__methods)
-
-  all_names <- union(member_names, method_names)
-  filtered_names <- findMatches(pattern, all_names)
-
-  filtered_names[!startsWith(filtered_names, "_")]
-}
-
-#' @export
 print.polars_expr <- function(x, ...) {
   x$`_rexpr`$as_str() |>
     writeLines()
