41fd6d7bf6925a07f54f0d5ae691d87698a37d17
eitsupi <ts1s1andn@gmail.com>
2025-03-03 12:59:39 +0000

    wip pref: Use `$` method for dataframe

--- R/expr-s3-base.R
+++ R/expr-s3-base.R
@@ -16,7 +16,7 @@
 }
 
 #' @export
-`.DollarNames.polars_expr` <- function(x, pattern = "") {
+.DollarNames.polars_expr <- function(x, pattern = "") {
   member_names <- ls(x)
   method_names <- names(polars_expr__methods)
 
