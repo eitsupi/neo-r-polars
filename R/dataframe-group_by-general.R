6b3041cebb7d0e3e3e3c1344b94bcb82c9bceebd
etiennebacher <etienne.bacher@protonmail.com>
2025-04-23 11:30:00 +0200

    lintr

--- R/dataframe-group_by-general.R
+++ R/dataframe-group_by-general.R
@@ -4,7 +4,7 @@ polars_groupby__methods <- new.env(parent = emptyenv())
 wrap_to_group_by <- function(x, by, maintain_order) {
   if (has_name(by, "maintain_order")) {
     warn(
-      "In `$group_by()`, `...` contain an argument named `maintain_order`. Did you mean `.maintain_order` instead?"
+      "In `$group_by()`, `...` contain an argument named `maintain_order`. Did you mean `.maintain_order` instead?" # nolint
     )
   }
   self <- new.env(parent = emptyenv())
