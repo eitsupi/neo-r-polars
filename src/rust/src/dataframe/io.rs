e87d652d397a0e791548375a0cc04d4271230036
eitsupi <ts1s1andn@gmail.com>
2025-05-25 07:51:28 +0000

    fix: the csv, ipc, and ipc_streaming features are no longer available for emscripten

--- src/rust/src/dataframe/io.rs
+++ src/rust/src/dataframe/io.rs
@@ -13,40 +13,54 @@ impl PlRDataFrame {
         n_rows: Option<NumericScalar>,
         row_index_name: Option<&str>,
     ) -> Result<Self> {
-        let row_index_offset = <Wrap<u32>>::try_from(row_index_offset)?.0;
-        let columns: Option<Vec<String>> = columns.map(|x| x.iter().map(|x| x.into()).collect());
-        let projection = projection
-            .map(|x| <Wrap<Vec<usize>>>::try_from(x).map(|x| x.0))
-            .transpose()?;
-        let n_rows = n_rows
-            .map(|x| <Wrap<usize>>::try_from(x).map(|x| x.0))
-            .transpose()?;
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            let row_index_offset = <Wrap<u32>>::try_from(row_index_offset)?.0;
+            let columns: Option<Vec<String>> =
+                columns.map(|x| x.iter().map(|x| x.into()).collect());
+            let projection = projection
+                .map(|x| <Wrap<Vec<usize>>>::try_from(x).map(|x| x.0))
+                .transpose()?;
+            let n_rows = n_rows
+                .map(|x| <Wrap<usize>>::try_from(x).map(|x| x.0))
+                .transpose()?;
 
-        let row_index = row_index_name.map(|x| RowIndex {
-            name: x.into(),
-            offset: row_index_offset,
-        });
-        let file = std::fs::File::open(source).map_err(RPolarsErr::from)?;
+            let row_index = row_index_name.map(|x| RowIndex {
+                name: x.into(),
+                offset: row_index_offset,
+            });
+            let file = std::fs::File::open(source).map_err(RPolarsErr::from)?;
 
-        let df = IpcStreamReader::new(file)
-            .with_projection(projection)
-            .with_columns(columns)
-            .with_n_rows(n_rows)
-            .with_row_index(row_index)
-            .set_rechunk(rechunk)
-            .finish()
-            .map_err(RPolarsErr::from)?;
-        Ok(df.into())
+            let df = IpcStreamReader::new(file)
+                .with_projection(projection)
+                .with_columns(columns)
+                .with_n_rows(n_rows)
+                .with_row_index(row_index)
+                .set_rechunk(rechunk)
+                .finish()
+                .map_err(RPolarsErr::from)?;
+            Ok(df.into())
+        }
+        #[cfg(target_arch = "wasm32")]
+        {
+            Err(RPolarsErr::Other(format!("Not supported in WASM")).into())
+        }
     }
 
-    #[cfg(not(target_arch = "wasm32"))]
     pub fn write_json(&mut self, path: &str) -> Result<()> {
-        let f = std::fs::File::create(path).map_err(RPolarsErr::from)?;
+        #[cfg(not(target_arch = "wasm32"))]
+        {
+            let f = std::fs::File::create(path).map_err(RPolarsErr::from)?;
 
-        JsonWriter::new(f)
-            .with_json_format(JsonFormat::Json)
-            .finish(&mut self.df)
-            .map_err(RPolarsErr::from)?;
-        Ok(())
+            JsonWriter::new(f)
+                .with_json_format(JsonFormat::Json)
+                .finish(&mut self.df)
+                .map_err(RPolarsErr::from)?;
+            Ok(())
+        }
+        #[cfg(target_arch = "wasm32")]
+        {
+            Err(RPolarsErr::Other(format!("Not supported in WASM")).into())
+        }
     }
 }
