a331067b8e906890a5c9f8ef9ecdecc36bc5a8dd
eitsupi <ts1s1andn@gmail.com>
2025-05-30 13:48:56 +0000

    fix: Ensure mirai >= 2.3.0

--- R/integration-mirai.R
+++ R/integration-mirai.R
@@ -2,11 +2,13 @@
 # https://github.com/tidyverse/glue/blob/a3f80d678274ef634c10c2cb094c939b1543222a/R/zzz.R#L4-L11
 
 register_mirai_serial <- function() {
-  mirai::register_serial(
-    c("polars_data_frame", "polars_lazy_frame", "polars_series"),
-    sfunc = list(\(x) x$serialize(), \(x) x$serialize(), \(x) x$serialize()),
-    ufunc = list(pl$deserialize_df, pl$deserialize_lf, pl$deserialize_series)
-  )
+  if (is_installed("mirai", version = "2.3.0")) {
+    mirai::register_serial(
+      c("polars_data_frame", "polars_lazy_frame", "polars_series"),
+      sfunc = list(\(x) x$serialize(), \(x) x$serialize(), \(x) x$serialize()),
+      ufunc = list(pl$deserialize_df, pl$deserialize_lf, pl$deserialize_series)
+    )
+  }
 }
 
 on_load({
