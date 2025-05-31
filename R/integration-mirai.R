24f9a5ed51e5828c6e17f2f4820bef52a693c680
eitsupi <ts1s1andn@gmail.com>
2025-05-31 09:02:54 +0000

    perf: simplify

--- R/integration-mirai.R
+++ R/integration-mirai.R
@@ -2,7 +2,9 @@
 # https://github.com/tidyverse/glue/blob/a3f80d678274ef634c10c2cb094c939b1543222a/R/zzz.R#L4-L11
 
 register_mirai_serial <- function() {
-  if (is_installed("mirai", version = "2.3.0")) {
+  # If the mirai package is not installed, `asNamespace("mirai")` will throw an error,
+  # so this function should only be called when the mirai package is loaded.
+  if (exists("register_serial", envir = asNamespace("mirai"), mode = "function")) {
     mirai::register_serial(
       c("polars_data_frame", "polars_lazy_frame", "polars_series"),
       sfunc = list(\(x) x$serialize(), \(x) x$serialize(), \(x) x$serialize()),
