726c8bdb171f6676b1879de7efd040baf0e679d6
etiennebacher <etienne.bacher@protonmail.com>
2025-06-09 17:18:13 +0200

    typo

--- tests/testthat/test-utils-parse-expr.R
+++ tests/testthat/test-utils-parse-expr.R
@@ -1,4 +1,4 @@
-test_that("clear error message when passing lists or vectors to dynamic dots", {
+test_that("clear error message when passing lists with some Polars expr to dynamic dots", {
   dat <- pl$DataFrame(a = 1:2, b = 3:4)
   exprs <- list("a", pl$col("b") + 1)
   expect_snapshot(
