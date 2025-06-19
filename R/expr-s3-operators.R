0eb177172332883f53be17a107c8fd0438e5505a
etiennebacher <etienne.bacher@protonmail.com>
2025-06-19 23:13:09 +0200

    drop logical operators from Rd page

--- R/expr-s3-operators.R
+++ R/expr-s3-operators.R
@@ -105,49 +105,41 @@ NULL
 }
 
 #' @export
-#' @rdname s3-arithmetic
 `>.polars_expr` <- function(e1, e2) {
   as_polars_expr(e1, as_lit = TRUE)$gt(e2)
 }
 
 #' @export
-#' @rdname s3-arithmetic
 `<=.polars_expr` <- function(e1, e2) {
   as_polars_expr(e1, as_lit = TRUE)$le(e2)
 }
 
 #' @export
-#' @rdname s3-arithmetic
 `>=.polars_expr` <- function(e1, e2) {
   as_polars_expr(e1, as_lit = TRUE)$ge(e2)
 }
 
 #' @export
-#' @rdname s3-arithmetic
 `==.polars_expr` <- function(e1, e2) {
   as_polars_expr(e1, as_lit = TRUE)$eq(e2)
 }
 
 #' @export
-#' @rdname s3-arithmetic
 `!=.polars_expr` <- function(e1, e2) {
   as_polars_expr(e1, as_lit = TRUE)$ne(e2)
 }
 
 #' @export
-#' @rdname s3-arithmetic
 `!.polars_expr` <- function(e1) {
   as_polars_expr(e1, as_lit = TRUE)$invert()
 }
 
 #' @export
-#' @rdname s3-arithmetic
 `&.polars_expr` <- function(e1, e2) {
   as_polars_expr(e1, as_lit = TRUE)$and(e2)
 }
 
 #' @export
-#' @rdname s3-arithmetic
 `|.polars_expr` <- function(e1, e2) {
   as_polars_expr(e1, as_lit = TRUE)$or(e2)
 }
